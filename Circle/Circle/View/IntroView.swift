11ed9f4762694e41470cbb3f7b733949aa8ab879
grgnjhyxxk <grgnjhyxxk@gmail.com>
2023-10-11 19:45:35 +0900

    [✏️  코드 작성] IntroView, SpinningCircleView 추가

--- Circle/Circle/View/IntroView.swift
+++ Circle/Circle/View/IntroView.swift
@@ -5,4 +5,45 @@
 //  Created by Jaehyeok Lim on 10/11/23.
 //
 
-import Foundation
+import UIKit
+
+class IntroView: UIView {
+
+}
+
+class SpinningCirclesView: UIView {
+
+    var bigCircleView: UIView!
+    var smallCircleViews: [UIView] = []
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupViews()
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        setupViews()
+    }
+
+    private func setupViews() {
+        bigCircleView = UIView(frame: CGRect(x: 0, y: 0, width: 80, height: 80))
+        bigCircleView.center = center
+        bigCircleView.backgroundColor = .white
+        bigCircleView.layer.cornerRadius = 40
+        addSubview(bigCircleView)
+
+        let radius: CGFloat = 70
+        let angleStep = CGFloat(2 * Double.pi / 8)
+
+        for i in 0..<8 {
+            let angle = CGFloat(i) * angleStep
+            let smallCircleView = UIView(frame: CGRect(x: 0, y: 0, width: 18, height: 18))
+            smallCircleView.backgroundColor = .white
+            smallCircleView.layer.cornerRadius = 9
+            smallCircleView.center = CGPoint(x: bigCircleView.center.x + radius * cos(angle), y: bigCircleView.center.y + radius * sin(angle))
+            addSubview(smallCircleView)
+            smallCircleViews.append(smallCircleView)
+        }
+    }
+}
