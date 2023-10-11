5bc431d8dc28367fe8c73d6a2240e1f4e403ede3
grgnjhyxxk <grgnjhyxxk@gmail.com>
2023-10-12 01:51:42 +0900

    [✏️  코드 수정] 기본 ViewLayout 환경 추가

--- Circle/Circle/ViewController/MainViewController.swift
+++ Circle/Circle/ViewController/MainViewController.swift
@@ -5,4 +5,30 @@
 //  Created by Jaehyeok Lim on 10/12/23.
 //
 
-import Foundation
+import UIKit
+import SnapKit
+
+class MainViewController: UIViewController {
+    
+    var viewList: [UIView] = []
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        addOnView()
+        viewLayout()
+    }
+    
+    private func addOnView() {
+        viewList = []
+        
+        for uiView in viewList {
+            view.addSubview(uiView)
+        }
+    }
+    
+    private func viewLayout() {
+        view.backgroundColor = .black
+    }
+}
+
