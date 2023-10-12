3d6536a66afda7bddecea15246f563e8a65072f6
grgnjhyxxk <grgnjhyxxk@gmail.com>
2023-10-12 18:52:26 +0900

    [✏️  코드 작성] collectionView 추가 및 UPCarouselFlowLayout 레이아웃 사용

--- Circle/Circle/ViewController/MainViewController.swift
+++ Circle/Circle/ViewController/MainViewController.swift
@@ -7,28 +7,69 @@
 
 import UIKit
 import SnapKit
+import UPCarouselFlowLayout
 
 class MainViewController: UIViewController {
     
-    var viewList: [UIView] = []
+    private var viewList: [UIView] = []
+
+    private var collectionView: UICollectionView!
+    private let cellIdentifier = "MainViewCustomCollectionCell"
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        setupCollectionView()
         addOnView()
         viewLayout()
     }
-    
-    private func addOnView() {
-        viewList = []
+
+    private func setupCollectionView() {
+        let layout = UPCarouselFlowLayout()
         
+        layout.scrollDirection = .horizontal
+
+        collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        collectionView.register(MainViewCustomCollectionCell.self, forCellWithReuseIdentifier: cellIdentifier)
+        collectionView.backgroundColor = .clear
+
+        collectionView.delegate = self
+        collectionView.dataSource = self
+    }
+
+    private func addOnView() {
+        viewList = [collectionView]
+
         for uiView in viewList {
             view.addSubview(uiView)
         }
     }
-    
+
     private func viewLayout() {
         view.backgroundColor = .black
+        
+        collectionView.snp.makeConstraints { make in
+            make.leading.trailing.equalToSuperview()
+            make.bottom.equalToSuperview().offset(-50)
+            make.height.equalTo(100)
+        }
     }
 }
 
+extension MainViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 7
+    }
+
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellIdentifier, for: indexPath) as! MainViewCustomCollectionCell
+
+        return cell
+    }
+}
+
+extension MainViewController: UICollectionViewDelegate {
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+
+    }
+}
