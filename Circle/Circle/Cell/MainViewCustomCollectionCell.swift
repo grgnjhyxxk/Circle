384cc00080845e6b30d57ae42c0b9b8bc3dbfeb3
grgnjhyxxk <grgnjhyxxk@gmail.com>
2023-10-12 18:50:52 +0900

    [✏️  코드 작성] MainViewCustomCollectionCell 기본 코드 작성 (테스트용)

--- Circle/Circle/Cell/MainViewCustomCollectionCell.swift
+++ Circle/Circle/Cell/MainViewCustomCollectionCell.swift
@@ -5,4 +5,35 @@
 //  Created by Jaehyeok Lim on 10/12/23.
 //
 
-import Foundation
+import UIKit
+import SnapKit
+
+class MainViewCustomCollectionCell: UICollectionViewCell {
+    
+    let imageView: UIImageView = {
+        let imageView = UIImageView()
+        
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.layer.cornerRadius = 40
+        imageView.clipsToBounds = true
+        imageView.backgroundColor = UIColor(white: 1, alpha: 0.75)
+        
+        return imageView
+    }()
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+
+        addSubview(imageView)
+
+        imageView.snp.makeConstraints { make in
+            make.center.equalToSuperview()
+            make.size.equalTo(80)
+        }
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
