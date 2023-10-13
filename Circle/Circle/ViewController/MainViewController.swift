95f30551af1de3d9f69f26d175fd6fac484369ce
grgnjhyxxk <grgnjhyxxk@gmail.com>
2023-10-13 19:02:20 +0900

    [✏️  코드 작성] spinningCirclesView 및 Navigation Item, titlaLabelList와 MainViewTitleLabel 그리고 그에 맞는 Cell 기능 추가

--- Circle/Circle/ViewController/MainViewController.swift
+++ Circle/Circle/ViewController/MainViewController.swift
@@ -12,11 +12,15 @@ import UPCarouselFlowLayout
 class MainViewController: UIViewController {
     
     private var viewList: [UIView] = []
+    private let titleLabelList: [String] = ["Vitality", "Radiance", "Clarity", "Balance", "Calm", "Harmony", "Serenity", "Purity"]
+    var colors: [UIColor] = [UIColor.red, UIColor.orange, UIColor.yellow, UIColor.green, UIColor.blue, UIColor.purple, UIColor.black, UIColor.white]
 
     private var spinningCirclesView = SpinningCirclesView()
 
     private var collectionView: UICollectionView!
     private let cellIdentifier = "MainViewCustomCollectionCell"
+    
+    private var MainViewTitleLabel: UILabel = MainView().MainViewTitleLabel()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,23 +28,32 @@ class MainViewController: UIViewController {
         setupCollectionView()
         addOnView()
         viewLayout()
+        spinningCirclesView.startAnimation()
+ 
+        let backButton = UIBarButtonItem(image: UIImage(systemName: "chevron.backward"), style: .plain, target: self, action: #selector(backButtonAction))
+        backButton.tintColor = UIColor.white
+        navigationItem.leftBarButtonItem = backButton
+        
+        navigationItem.backBarButtonItem = UIBarButtonItem(title: "", style: .plain, target: nil, action: nil)
     }
 
     private func setupCollectionView() {
         let layout = UPCarouselFlowLayout()
         
         layout.scrollDirection = .horizontal
-
+        
         collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
         collectionView.register(MainViewCustomCollectionCell.self, forCellWithReuseIdentifier: cellIdentifier)
         collectionView.backgroundColor = .clear
-
+        
         collectionView.delegate = self
         collectionView.dataSource = self
+    
+        collectionView.isScrollEnabled = false
     }
 
     private func addOnView() {
-        viewList = [spinningCirclesView, collectionView]
+        viewList = [spinningCirclesView, collectionView, MainViewTitleLabel]
 
         for uiView in viewList {
             view.addSubview(uiView)
@@ -54,7 +67,7 @@ class MainViewController: UIViewController {
         
         spinningCirclesView.snp.makeConstraints { make in
             make.centerX.equalToSuperview()
-            make.top.equalToSuperview().offset(90)
+            make.top.equalToSuperview().offset(85)
         }
         
         collectionView.snp.makeConstraints { make in
@@ -62,23 +75,42 @@ class MainViewController: UIViewController {
             make.bottom.equalToSuperview().offset(-50)
             make.height.equalTo(100)
         }
+        
+        MainViewTitleLabel.text = titleLabelList[0]
+        
+        MainViewTitleLabel.snp.makeConstraints { make in
+            make.centerX.equalToSuperview()
+            make.top.equalTo(spinningCirclesView.snp.bottom).offset(35)
+        }
+    }
+    
+    @objc func backButtonAction() {
+        navigationController?.popViewController(animated: true)
     }
 }
 
 extension MainViewController: UICollectionViewDataSource {
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 7
+        return colors.count
     }
 
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellIdentifier, for: indexPath) as! MainViewCustomCollectionCell
-
+        
+        if indexPath.row == 6 {
+            cell.imageView.layer.borderColor = UIColor.white.withAlphaComponent(0.75).cgColor
+            cell.imageView.layer.borderWidth = 1.5
+        }
+        
+        cell.imageView.backgroundColor = colors[indexPath.row].withAlphaComponent(0.75)
+        
         return cell
     }
 }
 
 extension MainViewController: UICollectionViewDelegate {
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-
+        collectionView.scrollToItem(at: indexPath, at: .centeredHorizontally, animated: true)
+        MainViewTitleLabel.text = titleLabelList[indexPath.row]
     }
 }
