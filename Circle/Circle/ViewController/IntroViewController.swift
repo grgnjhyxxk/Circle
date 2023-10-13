60cbc448b9c47c0b009b5b708f3d724fc7087e37
grgnjhyxxk <grgnjhyxxk@gmail.com>
2023-10-13 19:00:54 +0900

    [✏️  코드 수정] spinningCirclesView.startAnimation()

--- Circle/Circle/ViewController/IntroViewController.swift
+++ Circle/Circle/ViewController/IntroViewController.swift
@@ -23,13 +23,13 @@ class IntroViewController: UIViewController {
 
         addOnView()
         viewLayout()
-        startAnimation()
+        spinningCirclesView.startAnimation()
         addTargets()
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-        startAnimation()
+        spinningCirclesView.startAnimation()
     }
     
     private func addOnView() {
@@ -76,28 +76,6 @@ class IntroViewController: UIViewController {
         
         if let navigationController = self.view.window?.rootViewController as? UINavigationController {
             navigationController.pushViewController(viewController, animated: true)
-        }
-    }
-
-    
-    private func startAnimation() {
-        let animation = CAKeyframeAnimation(keyPath: "transform.rotation.z")
-        animation.values = [0, CGFloat.pi / 4, CGFloat.pi / 2, CGFloat.pi * 3 / 4, CGFloat.pi, CGFloat.pi * 5 / 4, CGFloat.pi * 3 / 2, CGFloat.pi * 7 / 4, CGFloat.pi * 2]
-        animation.keyTimes = [0, 0.125, 0.25, 0.375, 0.5, 0.625, 0.75, 0.875, 1]
-        animation.duration = 10.0
-        animation.repeatCount = .infinity
-
-        spinningCirclesView.layer.add(animation, forKey: nil)
-
-        for (index, smallCircle) in spinningCirclesView.smallCircleViews.enumerated() {
-            let smallAnimation = CAKeyframeAnimation(keyPath: "transform.rotation.z")
-            smallAnimation.values = animation.values
-            smallAnimation.keyTimes = animation.keyTimes
-            smallAnimation.duration = animation.duration
-            smallAnimation.repeatCount = .infinity
-            smallAnimation.beginTime = CACurrentMediaTime() + Double(index) * 0.125
-            
-            smallCircle.layer.add(smallAnimation, forKey: nil)
         }
     }
 }
