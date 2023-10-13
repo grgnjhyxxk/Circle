99a628dfc2c85e3b6948bd302df0d699099f2fe4
grgnjhyxxk <grgnjhyxxk@gmail.com>
2023-10-13 18:59:46 +0900

    [✏️  코드 작성] MainViewTitleLabel 추가

--- Circle/Circle/View/MainView.swift
+++ Circle/Circle/View/MainView.swift
@@ -5,4 +5,17 @@
 //  Created by Jaehyeok Lim on 10/12/23.
 //
 
-import Foundation
+import UIKit
+
+class MainView: UIView {
+    
+    func MainViewTitleLabel() -> UILabel {
+        let label = UILabel()
+        
+        label.text = "Circle"
+        label.textColor = UIColor.white
+        label.font = UIFont(name: "NotoSans-Thin", size: 30)
+        
+        return label
+    }
+}
