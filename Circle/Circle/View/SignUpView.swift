ce77ce28f4cf9dd19a74bfaf022f7d2523f5e72c
grgnjhyxxk <grgnjhyxxk@gmail.com>
2023-10-16 23:22:48 +0900

    [✏️  코드 작성] mainTitleLabel, subTitleLabel, mainTextField 추가

--- Circle/Circle/View/SignUpView.swift
+++ Circle/Circle/View/SignUpView.swift
@@ -5,4 +5,42 @@
 //  Created by Jaehyeok Lim on 10/16/23.
 //
 
-import Foundation
+import UIKit
+
+class SignUpView: UIView {
+    
+    func mainTitleLabel() -> UILabel {
+        let label = UILabel()
+        
+        label.text = ""
+        label.textColor = UIColor.white
+        label.font = UIFont(name: "NotoSans-Thin", size: 24)
+        
+        return label
+    }
+    
+    func subTitleLabel() -> UILabel {
+        let label = UILabel()
+        
+        label.numberOfLines = 0
+        label.textAlignment = .center
+        label.text = ""
+        label.textColor = UIColor.white
+        label.font = UIFont(name: "NotoSans-ExtraLight", size: 12)
+        
+        return label
+    }
+    
+    func mainTextField() -> UITextField {
+        let textField = UITextField()
+        
+        textField.font = UIFont(name: "NotoSans-ExtraLight", size: 13)
+        textField.textColor = UIColor.white
+        textField.backgroundColor = UIColor.white.withAlphaComponent(0.2)
+        textField.layer.cornerRadius = 5
+        textField.leftView = UIView(frame: CGRect(x: 0, y: 0, width: 10, height: textField.frame.height))
+        textField.leftViewMode = .always
+        
+        return textField
+    }
+}
