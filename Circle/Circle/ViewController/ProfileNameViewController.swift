f5c2f00e2cbbbf3841cbdf72dc56c4686709fe87
grgnjhyxxk <grgnjhyxxk@gmail.com>
2023-10-18 10:04:36 +0900

    [✏️  코드 작성] nextButtonAction 기능 수정 및 활성화를 위한 updatedText 추가

--- Circle/Circle/ViewController/ProfileNameViewController.swift
+++ Circle/Circle/ViewController/ProfileNameViewController.swift
@@ -26,15 +26,18 @@ class ProfileNameViewController: BaseSignUpViewController {
         } else {
             let viewController = UserNameViewController()
             
-            errorTextLabel.alpha = 0
-            
             if let navigationController = self.view.window?.rootViewController as? UINavigationController {
+                errorTextLabel.alpha = 0
+                mainTextField.layer.borderWidth = 0.5
+                mainTextField.layer.borderColor = UIColor.white.withAlphaComponent(0.1).cgColor
+                
                 navigationController.pushViewController(viewController, animated: true)
             }
         }
     }
     
     func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        let updatedText = (textField.text as NSString?)?.replacingCharacters(in: range, with: string) ?? ""
         let allowedCharacters = CharacterSet(charactersIn: "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789_.")
         let characterSet = CharacterSet(charactersIn: string)
         
@@ -45,7 +48,8 @@ class ProfileNameViewController: BaseSignUpViewController {
             self.mainTextField.layer.borderColor = UIColor.white.withAlphaComponent(0.1).cgColor
             
             self.errorTextLabel.alpha = 0
-
+            
+            nextButton.isEnabled = !updatedText.isEmpty
         } else {
             print("잘못된 문자입니다.")
             
