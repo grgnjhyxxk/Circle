33c86f7d26a08b87ccfeb4caf640563f4efa01b7
grgnjhyxxk <grgnjhyxxk@gmail.com>
2023-10-18 10:01:09 +0900

    [✏️  코드 작성] 비밀번호 설정 뷰 컨트롤러와 이메일 인증 컨트롤러에서 쓰일 checkButton, blurView  추가

--- Circle/Circle/View/SignUpView.swift
+++ Circle/Circle/View/SignUpView.swift
@@ -55,4 +55,27 @@ class SignUpView: UIView {
         
         return label
     }
+    
+    func checkButton() -> UIButton {
+        let button = UIButton()
+        
+        button.setTitle("인증", for: .normal)
+        button.setTitleColor(UIColor.white.withAlphaComponent(0.5), for: .normal)
+        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 17)
+        button.backgroundColor = UIColor.white.withAlphaComponent(0.08)
+        button.layer.cornerRadius = 5
+        button.layer.borderWidth = 0.5
+        button.layer.borderColor = UIColor.white.withAlphaComponent(0.1).cgColor
+        
+        return button
+    }
+    
+    func blurView() -> UIView {
+        let view = UIView()
+        
+        view.backgroundColor = UIColor.black.withAlphaComponent(0.5)
+        view.layer.cornerRadius = 5
+
+        return view
+    }
 }
