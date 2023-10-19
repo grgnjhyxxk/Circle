492e31c5d374d146decc7bbc17efa289775b6978
grgnjhyxxk <grgnjhyxxk@gmail.com>
2023-10-19 17:53:43 +0900

    [✏️ 코드 작성] 기본 ViewLayout 환경 추가

--- Circle/Circle/ViewController/MainViewController.swift
+++ Circle/Circle/ViewController/MainViewController.swift
@@ -7,29 +7,60 @@
 
 import UIKit
 import SnapKit
-import UPCarouselFlowLayout
 
 class MainViewController: UIViewController {
-    
+
     private var viewList: [UIView] = []
+    private var contentViewList: [UIView] = []
+    
+    private var scrollView: UIScrollView = UIScrollView()
+    private var contentView: UIView = UIView()
+    
+    private var vivivi: UIView = UIView()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         addOnView()
         viewLayout()
+        addOnContentView()
+        contentViewLayout()
+        
+        scrollView.backgroundColor = UIColor.black
+        contentView.backgroundColor = UIColor.black
     }
 
     private func addOnView() {
-        viewList = []
-
+        viewList = [scrollView]
+        
         for uiView in viewList {
             view.addSubview(uiView)
         }
+        
+        scrollView.addSubview(contentView)
     }
 
     private func viewLayout() {
-        view.backgroundColor = .black
-    }
+        scrollView.snp.makeConstraints { make in
+            make.edges.equalToSuperview()
+        }
         
+        contentView.snp.makeConstraints { make in
+            make.edges.equalToSuperview()
+            make.width.equalToSuperview()
+            make.height.equalToSuperview().multipliedBy(1000)
+        }
+    }
+    
+    private func addOnContentView() {
+        contentViewList = [vivivi]
+        
+        for uiView in contentViewList {
+            contentView.addSubview(uiView)
+        }
+    }
+    
+    private func contentViewLayout() {
+    }
 }
+
