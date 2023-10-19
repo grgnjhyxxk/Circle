8b60a27a666f9d668b36dc2aee6d333b20e07093
grgnjhyxxk <grgnjhyxxk@gmail.com>
2023-10-19 17:35:16 +0900

    [📄 파일 생성] Model과 Data관련 기능을 위한 DataManager 생성

---
+++ Circle/Circle/DataManager.swift
@@ -0,0 +1,21 @@
+//
+//  DataManager.swift
+//  Circle
+//
+//  Created by Jaehyeok Lim on 10/18/23.
+//
+
+import UIKit
+
+struct UserData {
+    var profileName: String
+    var userName: String
+    var password: String
+    var email: String?
+    var phoneNumber: String?
+    var image: String?
+    var birth: String?
+    var gender: String?
+    var userID: String?
+}
+
