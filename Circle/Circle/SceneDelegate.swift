0b761b3f15c0b43e22a0f4efd96a264fc061f27a
grgnjhyxxk <grgnjhyxxk@gmail.com>
2023-10-19 17:43:28 +0900

    [✏️  코드 작성] isLoggedIn() 추가

--- Circle/Circle/SceneDelegate.swift
+++ Circle/Circle/SceneDelegate.swift
@@ -19,11 +19,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         window = UIWindow(windowScene: windowScene)
         window?.makeKeyAndVisible()
-        
-        let introViewController = IntroViewController()
-        let navigationController = UINavigationController(rootViewController: introViewController)
-        
-        window?.rootViewController = navigationController
+        window?.rootViewController = IntroViewController().isLoggedIn()
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
