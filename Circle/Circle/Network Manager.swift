4a13b5a4945fde79cf040e31ca5cbedfe161536a
grgnjhyxxk <grgnjhyxxk@gmail.com>
2023-10-20 23:24:02 +0900

    [✏️  코드 수정] 서버 데이터베이스 저장 위치 변경 및 프로퍼티 이름 변경에 따른 수정

--- Circle/Circle/Network Manager.swift
+++ Circle/Circle/Network Manager.swift
@@ -18,20 +18,20 @@ func signUpDataUploadServer(userData: UserData, completion: @escaping (Bool, Err
         "userName": userData.userName,
         "password": userData.password,
         "myCircleDigits": userData.myCircleDigits,
-        "myinTheCircleDigits": userData.MyinTheCircleDigits,
-        "myPostDigits": userData.MyPostDigits,
+        "myInTheCircleDigits": userData.myInTheCircleDigits,
+        "myPostDigits": userData.myPostDigits,
         "followerDigits": userData.followerDigits,
         "followingDigits": userData.followingDigits,
         "socialValidation": userData.socialValidation,
-        "intrduction": userData.intrduction ?? "", // intrduction 추가
+        "intrduction": userData.intrduction ?? "",
         "email": userData.email ?? "",
         "phoneNumber": userData.phoneNumber ?? "",
         "image": userData.image ?? "",
         "birth": userData.birth ?? "",
         "gender": userData.gender ?? ""
     ]
-
-    dataBase.collection("Users").document(userData.profileName.lowercased()).setData(data, merge: true) { error in
+    
+    dataBase.collection("dataBase").document("users").collection(userData.profileName.lowercased()).document("info").setData(data, merge: true) { error in
         if let error = error {
             completion(false, error)
         } else {
@@ -43,7 +43,7 @@ func signUpDataUploadServer(userData: UserData, completion: @escaping (Bool, Err
 func fetchUserData(profileName: String, completion: @escaping (UserData?, Error?) -> Void) {
     let dataBase = Firestore.firestore()
 
-    let docRef = dataBase.collection("Users").document(profileName.lowercased())
+    let docRef = dataBase.collection("dataBase").document("users").collection(profileName.lowercased()).document("info")
 
     docRef.getDocument { (document, error) in
         if let document = document, document.exists {
@@ -53,12 +53,12 @@ func fetchUserData(profileName: String, completion: @escaping (UserData?, Error?
                     userName: data["userName"] as? String ?? "",
                     password: data["password"] as? String ?? "",
                     myCircleDigits: data["myCircleDigits"] as? Int ?? 0,
-                    MyinTheCircleDigits: data["myinTheCircleDigits"] as? Int ?? 0,
-                    MyPostDigits: data["myPostDigits"] as? Int ?? 0,
+                    myInTheCircleDigits: data["myInTheCircleDigits"] as? Int ?? 0,
+                    myPostDigits: data["myPostDigits"] as? Int ?? 0,
                     followerDigits: data["followerDigits"] as? Int ?? 0,
                     followingDigits: data["followingDigits"] as? Int ?? 0,
                     socialValidation: data["socialValidation"] as? Bool ?? false,
-                    intrduction: data["intrduction"] as? String, // intrduction 추가
+                    intrduction: data["intrduction"] as? String,
                     email: data["email"] as? String,
                     phoneNumber: data["phoneNumber"] as? String,
                     image: data["image"] as? String,
@@ -66,7 +66,7 @@ func fetchUserData(profileName: String, completion: @escaping (UserData?, Error?
                     gender: data["gender"] as? String,
                     userID: data["userID"] as? String
                 )
-                userDataList.append(userData)
+                myDataList.append(userData)
                 
                 completion(userData, nil)
             } else {
@@ -79,9 +79,25 @@ func fetchUserData(profileName: String, completion: @escaping (UserData?, Error?
     }
 }
 
-
-
-
 func comparePasswords(inputPassword: String, savedPassword: String) -> Bool {
     return inputPassword == savedPassword
+}
+
+func checkIfProfileNameExists(_ profileName: String, completion: @escaping (Bool, Error?) -> Void) {
+    let dataBase = Firestore.firestore()
+    
+    let profileNameRef = dataBase.collection("dataBase").document("users").collection(profileName.lowercased())
+    
+    profileNameRef.getDocuments { (snapshot, error) in
+        if let error = error {
+            completion(false, error)
+            return
+        }
+        
+        if snapshot?.isEmpty == true {
+            completion(false, nil)
+        } else {
+            completion(true, nil)
+        }
+    }
 }
