12faeb1290ea49602174e0fcd9cb60abf75d6fb8
grgnjhyxxk <grgnjhyxxk@gmail.com>
2023-10-20 23:31:39 +0900

    [✏️ 코드 작성] 기본 ViewLayout 환경 추가

--- Circle/Circle/ViewController/Setting/SettingTableViewController.swift
+++ Circle/Circle/ViewController/Setting/SettingTableViewController.swift
@@ -5,4 +5,30 @@
 //  Created by Jaehyeok Lim on 10/20/23.
 //
 
-import Foundation
+import UIKit
+import SnapKit
+
+class SettingTableViewController: UIViewController {
+
+    private var viewList: [UIView] = []
+        
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        addOnView()
+        viewLayout()
+    }
+
+    private func addOnView() {
+        viewList = []
+        
+        for uiView in viewList {
+            view.addSubview(uiView)
+        }
+    }
+
+    private func viewLayout() {
+        view.backgroundColor = UIColor.black
+        
+    }
+}
