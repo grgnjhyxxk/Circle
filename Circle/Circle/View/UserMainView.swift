2702583040c71c86550739b090e8c34f7bdd9175
grgnjhyxxk <grgnjhyxxk@gmail.com>
2023-10-22 23:34:17 +0900

    [✏️  코드 작성] UserProfile UI 변경

--- Circle/Circle/View/UserMainView.swift
+++ Circle/Circle/View/UserMainView.swift
@@ -8,32 +8,26 @@
 import UIKit
 
 class UserMainView: UIView {
-    
-    func topView() -> UIView {
-        let view = UIView()
+    func profileNameButton() -> UIButton {
+        let button = UIButton()
         
-        view.backgroundColor = UIColor.black
+        button.setTitle("", for: .normal)
+        button.setTitleColor(UIColor.white, for: .normal)
+        button.titleLabel?.font = UIFont.systemFont(ofSize: 20, weight: .medium)
+        button.titleLabel?.textAlignment = .center
 
-        return view
+        return button
     }
-    
-    func profileNameTitleLabel() -> UILabel {
-        let label = UILabel()
-        
-        label.textColor = UIColor.white
-        label.font = UIFont.systemFont(ofSize: 18, weight: .medium)
-        label.textAlignment = .center
-        
-        return label
-    }
-    
+
     func userProfileImageView() -> UIImageView {
         let imageView = UIImageView()
         
         imageView.image = UIImage()
         imageView.backgroundColor = UIColor.white.withAlphaComponent(0.1)
         imageView.layer.cornerRadius = 45
+        imageView.layer.borderColor = UIColor.black.cgColor
         imageView.clipsToBounds = true
+
         return imageView
     }
     
@@ -41,7 +35,17 @@ class UserMainView: UIView {
         let label = UILabel()
         
         label.textColor = UIColor.white
-        label.font = UIFont.systemFont(ofSize: 14, weight: .medium)
+        label.font = UIFont.systemFont(ofSize: 16, weight: .medium)
+        label.textAlignment = .left
+        
+        return label
+    }
+    
+    func userCategoryTitleLabel() -> UILabel {
+        let label = UILabel()
+        
+        label.textColor = UIColor.white
+        label.font = UIFont.systemFont(ofSize: 16, weight: .medium)
         label.textAlignment = .left
         
         return label
@@ -52,57 +56,24 @@ class UserMainView: UIView {
         
         label.numberOfLines = 0
         label.textColor = UIColor.white
-        label.font = UIFont.systemFont(ofSize: 14, weight: .regular)
-        
-        return label
-    }
-    
-    func userMainStatusTitleLabel() -> UILabel {
-        let label = UILabel()
-        
-        label.textColor = UIColor.white
         label.font = UIFont.systemFont(ofSize: 14, weight: .light)
         label.textAlignment = .center
         
         return label
     }
-    
-    func userMainStatusDigitsLabel() -> UILabel {
-        let label = UILabel()
-        
-        label.textColor = UIColor.white
-        label.font = UIFont.systemFont(ofSize: 18, weight: .medium)
-        label.textAlignment = .center
-        
-        return label
-    }
-    
-    func userSubStatusTitleLabel() -> UILabel {
-        let label = UILabel()
-        
-        label.textColor = UIColor.white
-        label.font = UIFont.systemFont(ofSize: 14, weight: .light)
-        label.textAlignment = .center
-        
-        return label
-    }
-    
-    func userSubStatusDigitsLabel() -> UILabel {
-        let label = UILabel()
-        
-        label.textColor = UIColor.white
-        label.font = UIFont.systemFont(ofSize: 14, weight: .medium)
-        label.textAlignment = .center
-        
-        return label
-    }
-    
-    func socialValidationMarkButton() -> UIButton {
+
+    func subStatusButton() -> UIButton {
         let button = UIButton()
-        
-        button.setImage(UIImage(systemName: "checkmark.seal.fill"), for: .normal)
-        button.tintColor = UIColor.systemBlue
-        
+
+        button.titleLabel?.font = UIFont.systemFont(ofSize: 14, weight: .regular)
+        button.tintColor = UIColor.white
+        button.titleLabel?.textAlignment = .center
+        button.layer.cornerRadius = 15
+        button.layer.borderWidth = 0.5
+        button.layer.borderColor = UIColor.white.withAlphaComponent(0.5).cgColor
+        button.backgroundColor = UIColor.black
+//        button.sizeToFit()
+
         return button
     }
     
@@ -114,7 +85,6 @@ class UserMainView: UIView {
         button.titleLabel?.font = UIFont.systemFont(ofSize: 15, weight: .medium)
         button.titleLabel?.textAlignment = .center
         
-        // Add border to the bottom
         let border = CALayer()
         border.backgroundColor = UIColor.white.cgColor
         border.frame = CGRect(x: 0, y: button.frame.size.height - 1, width: button.frame.size.width, height: 1)
@@ -132,42 +102,17 @@ class UserMainView: UIView {
     }
     
     class MyProfileView: UIView {
-        
-        func settingListButton() -> UIButton {
-            let button = UIButton()
-            
-            button.setImage(UIImage(systemName: "line.3.horizontal"), for: .normal)
-            button.tintColor = UIColor.white
-            button.contentHorizontalAlignment = .fill
-            button.contentVerticalAlignment = .fill
-            
-            return button
-        }
-        
-        func postingButton() -> UIButton {
-            let button = UIButton()
-            
-            button.setImage(UIImage(systemName: "square.and.pencil"), for: .normal)
-            button.tintColor = UIColor.white
-            button.contentHorizontalAlignment = .fill
-            button.contentVerticalAlignment = .fill
-            
-            return button
-        }
-        
         func profileEditButton() -> UIButton {
             let button = UIButton()
             
-            button.setTitle("프로필 편집", for: .normal)
-            button.setTitleColor(UIColor.systemBlue, for: .normal)
-            button.titleLabel?.font = UIFont.systemFont(ofSize: 14, weight: .heavy)
+            button.setTitle("편집", for: .normal)
+            button.setTitleColor(UIColor.white, for: .normal)
+            button.titleLabel?.font = UIFont.systemFont(ofSize: 14, weight: .semibold)
             button.titleLabel?.textAlignment = .center
-            
-            button.tintColor = UIColor.white.withAlphaComponent(0.85)
-            button.imageEdgeInsets = UIEdgeInsets(top: 0, left: 4.5, bottom: 0, right: 0)
-            button.semanticContentAttribute = .forceRightToLeft
-            button.contentVerticalAlignment = .center
-            button.contentHorizontalAlignment = .leading
+            button.layer.cornerRadius = 15
+            button.layer.borderWidth = 0.5
+            button.layer.borderColor = UIColor.white.withAlphaComponent(0.5).cgColor
+            button.backgroundColor = UIColor.black
             
             return button
         }
--- Circle/Circle/ViewController/UserProfile/BasicUserProfileViewController.swift
+++ Circle/Circle/ViewController/UserProfile/BasicUserProfileViewController.swift
@@ -9,45 +9,31 @@ import UIKit
 import SnapKit
 
 class BasicUserProfileViewController: UIViewController {
-
+    
     var viewList: [UIView] = []
     var contentViewList: [UIView] = []
     
     var scrollView: UIScrollView = UIScrollView()
     var contentView: UIView = UIView()
-    var topView: UIView = UserMainView().topView()
     var topViewBottomSeparator: UIView = IntroView().separator()
     
-    var profileNameTitleLabel: UILabel = UserMainView().profileNameTitleLabel()
-    
-    var myCircleTitleLabel: UILabel = UserMainView().userMainStatusTitleLabel()
-    var inTheCircleTitleLabel: UILabel = UserMainView().userMainStatusTitleLabel()
-    var feedTitleLabel: UILabel = UserMainView().userMainStatusTitleLabel()
-
-    var myCircleDigitsLabel: UILabel = UserMainView().userMainStatusDigitsLabel()
-    var myInTheCircleDigitsLabel: UILabel = UserMainView().userMainStatusDigitsLabel()
-    var myPostDigitsLabel: UILabel = UserMainView().userMainStatusDigitsLabel()
+    var profileNameButton: UIButton = UserMainView().profileNameButton()
     
     var userNameTitleLabel: UILabel = UserMainView().userNameTitleLabel()
     var introductionLabel: UILabel = UserMainView().introductionLabel()
     
-    var followerLabel: UILabel = UserMainView().userSubStatusTitleLabel()
-    var followingLabel: UILabel = UserMainView().userSubStatusTitleLabel()
-    var followerDigitsLabel: UILabel = UserMainView().userSubStatusDigitsLabel()
-    var followingDigitsLabel: UILabel = UserMainView().userSubStatusDigitsLabel()
-
     var userProfileImageView: UIImageView = UserMainView().userProfileImageView()
     
-    var socialValidationMarkButton: UIButton = UserMainView().socialValidationMarkButton()
     var myPersonalPostsFeedButton: UIButton = UserMainView().selectMyPostFeedButton()
     var myCirclePostsFeedButton: UIButton = UserMainView().selectMyPostFeedButton()
     
     var myPersonalPostsFeedButtonBottomBar = UserMainView().selectMyPostFeedButtonBottomBar()
     var myCirclePostsButtonBottomBar = UserMainView().selectMyPostFeedButtonBottomBar()
-    
-    var settingListButton: UIButton = UserMainView.MyProfileView().settingListButton()
-    var postingButton: UIButton = UserMainView.MyProfileView().postingButton()
 
+    var profileEditButton: UIButton = UserMainView.MyProfileView().profileEditButton()
+    
+    var subStatusButton: UIButton = UserMainView().subStatusButton()
+    
     lazy var refreshControl: UIRefreshControl = {
         let refreshControl = UIRefreshControl()
         refreshControl.addTarget(self, action: #selector(refreshData), for: .valueChanged)
@@ -65,12 +51,11 @@ class BasicUserProfileViewController: UIViewController {
         contentViewLayout()
         contentViewSetting()
         addTargets()
+        navigationBarLayout()
     }
-
+    
     func addOnView() {
-        viewList = [scrollView, topView,
-                    profileNameTitleLabel,
-                    settingListButton, postingButton]
+        viewList = [scrollView]
         
         for uiView in viewList {
             view.addSubview(uiView)
@@ -78,46 +63,37 @@ class BasicUserProfileViewController: UIViewController {
         
         scrollView.addSubview(contentView)
     }
-
+    
     func viewLayout() {
         view.backgroundColor = UIColor.black
-        scrollView.backgroundColor = UIColor.clear
+        scrollView.backgroundColor = UIColor.black
         contentView.backgroundColor = UIColor.black
         
-        topView.snp.makeConstraints { make in
-            make.top.leading.trailing.equalToSuperview()
-            make.height.equalTo(90)
-        }
-        
-        profileNameTitleLabel.snp.makeConstraints { make in
-            make.leading.trailing.equalTo(topView)
-            make.top.equalTo(topView).offset(55)
-        }
-        
         scrollView.snp.makeConstraints { make in
-            make.top.equalTo(topView.snp.bottom)
+            make.top.equalTo(self.view.safeAreaLayoutGuide.snp.top) // 수정된 부분
             make.leading.trailing.bottom.equalToSuperview()
         }
         
         contentView.snp.makeConstraints { make in
-            make.top.equalTo(scrollView).offset(-40)
+            make.top.equalTo(scrollView)
             make.leading.trailing.bottom.equalToSuperview()
             make.width.equalToSuperview()
             make.height.equalTo(1000)
         }
         
         scrollView.refreshControl = refreshControl
+        
+        navigationController?.navigationBar.isTranslucent = false
+        navigationController?.navigationBar.shadowImage = UIImage()
     }
     
     func addOnContentView() {
         contentViewList = [userProfileImageView,
-                           myCircleTitleLabel, inTheCircleTitleLabel, feedTitleLabel,
-                           myCircleDigitsLabel, myInTheCircleDigitsLabel, myPostDigitsLabel,
                            userNameTitleLabel, introductionLabel,
-                           followerLabel, followerDigitsLabel, followingLabel, followingDigitsLabel,
-                           socialValidationMarkButton,
                            topViewBottomSeparator,
-                           myPersonalPostsFeedButton, myCirclePostsFeedButton]
+                           myPersonalPostsFeedButton, myCirclePostsFeedButton,
+                           profileEditButton,
+                           subStatusButton]
         
         for uiView in contentViewList {
             contentView.addSubview(uiView)
@@ -126,138 +102,39 @@ class BasicUserProfileViewController: UIViewController {
     
     func contentViewLayout() {
         userProfileImageView.snp.makeConstraints { make in
-            make.top.equalTo(contentView).offset(60)
-            make.leading.equalTo(contentView).offset(25)
+            make.top.equalTo(contentView).offset(20)
+            make.centerX.equalTo(contentView)
             make.size.equalTo(CGSize(width: 90, height: 90))
-        }
-        
-        feedTitleLabel.snp.makeConstraints { make in
-            make.centerY.equalTo(userProfileImageView).offset(10)
-            make.trailing.equalTo(contentView).offset(-35)
-        }
-        
-        myPostDigitsLabel.snp.makeConstraints { make in
-            make.centerX.equalTo(feedTitleLabel)
-            make.centerY.equalTo(userProfileImageView).offset(-10)
-        }
-        
-        inTheCircleTitleLabel.snp.makeConstraints { make in
-            make.centerY.equalTo(userProfileImageView).offset(10)
-            make.trailing.equalTo(feedTitleLabel.snp.leading).offset(-35)
-        }
-        
-        myInTheCircleDigitsLabel.snp.makeConstraints { make in
-            make.centerX.equalTo(inTheCircleTitleLabel)
-            make.centerY.equalTo(userProfileImageView).offset(-10)
-        }
-        
-        myCircleTitleLabel.snp.makeConstraints { make in
-            make.centerY.equalTo(userProfileImageView).offset(10)
-            make.trailing.equalTo(inTheCircleTitleLabel.snp.leading).offset(-35)
-        }
-        
-        myCircleDigitsLabel.snp.makeConstraints { make in
-            make.centerX.equalTo(myCircleTitleLabel)
-            make.centerY.equalTo(userProfileImageView).offset(-10)
         }
         
         userNameTitleLabel.snp.makeConstraints { make in
             make.top.equalTo(userProfileImageView.snp.bottom).offset(10)
-            make.leading.equalTo(userProfileImageView)
+            make.centerX.equalTo(contentView)
         }
         
         introductionLabel.snp.makeConstraints { make in
             make.top.equalTo(userNameTitleLabel.snp.bottom).offset(5)
-            make.leading.equalTo(userNameTitleLabel)
-            make.trailing.equalTo(contentView).offset(-25)
+            make.leading.equalTo(contentView).offset(20)
+            make.trailing.equalTo(contentView).offset(-20)
         }
         
-        if introductionLabel.text == "" || introductionLabel.text == nil || introductionLabel.text == "nil" {
-            followerLabel.snp.makeConstraints { make in
-                make.top.equalTo(userNameTitleLabel.snp.bottom).offset(10)
-                make.leading.equalTo(userNameTitleLabel)
-            }
-        } else if userNameTitleLabel.text == "" || userNameTitleLabel.text == nil || userNameTitleLabel.text == "nil" {
-            followerLabel.snp.makeConstraints { make in
-                make.top.equalTo(userProfileImageView.snp.bottom).offset(10)
-                make.leading.equalTo(userProfileImageView)
-            }
-        } else {
-            followerLabel.snp.makeConstraints { make in
-                make.top.equalTo(introductionLabel.snp.bottom).offset(10)
-                make.leading.equalTo(userNameTitleLabel)
-            }
+        subStatusButton.snp.makeConstraints { make in
+            make.top.equalTo(introductionLabel.snp.bottom).offset(20)
+            make.centerX.equalTo(contentView)
+            make.width.equalTo(subStatusButton.titleLabel!.intrinsicContentSize.width + 50)
+            make.height.equalTo(30)
         }
         
-        followerDigitsLabel.snp.makeConstraints { make in
-            make.top.equalTo(followerLabel)
-            make.leading.equalTo(followerLabel.snp.trailing).offset(5)
-        }
-        
-        followingLabel.snp.makeConstraints { make in
-            make.top.equalTo(followerDigitsLabel)
-            make.leading.equalTo(followerDigitsLabel.snp.trailing).offset(25)
-        }
-        
-        followingDigitsLabel.snp.makeConstraints { make in
-            make.top.equalTo(followingLabel)
-            make.leading.equalTo(followingLabel.snp.trailing).offset(5)
-        }
-        
-        socialValidationMarkButton.snp.makeConstraints { make in
-            make.leading.equalTo(userNameTitleLabel.snp.trailing).offset(5)
-            make.centerY.equalTo(userNameTitleLabel)
-            make.size.equalTo(CGSize(width: 17, height: 17))
-        }
-        
-        topViewBottomSeparator.snp.makeConstraints { make in
-            make.leading.trailing.equalTo(contentView)
-            make.top.equalTo(followerDigitsLabel.snp.bottom).offset(15)
-            make.height.equalTo(1)
-        }
-        
-        myPersonalPostsFeedButton.snp.makeConstraints { make in
-            make.top.equalTo(topViewBottomSeparator.snp.bottom)
-            make.leading.equalTo(contentView)
-            make.size.equalTo(CGSize(width: view.frame.width / 2, height: 40))
-        }
-        
-        myCirclePostsFeedButton.snp.makeConstraints { make in
-            make.top.equalTo(topViewBottomSeparator.snp.bottom)
-            make.trailing.equalTo(contentView)
-            make.size.equalTo(CGSize(width: view.frame.width / 2, height: 40))
-        }
-        
-        myPersonalPostsFeedButton.addSubview(myPersonalPostsFeedButtonBottomBar)
-        myCirclePostsFeedButton.addSubview(myCirclePostsButtonBottomBar)
-
-        myPersonalPostsFeedButtonBottomBar.snp.makeConstraints { make in
-            make.centerX.bottom.equalToSuperview()
-            make.size.equalTo(CGSize(width: 100, height: 2))
-        }
-        
-        myCirclePostsButtonBottomBar.snp.makeConstraints { make in
-            make.centerX.bottom.equalToSuperview()
-            make.size.equalTo(CGSize(width: 100, height: 2))
-        }
-        
-        topViewBottomSeparator.backgroundColor = UIColor.white.withAlphaComponent(0.05)
-        
-        myCircleTitleLabel.text = "마이 서클"
-        inTheCircleTitleLabel.text = "인더 서클"
-        feedTitleLabel.text = "게시물"
-        followerLabel.text = "팔로워"
-        followingLabel.text = "팔로잉"
         myPersonalPostsFeedButton.setTitle("게시물", for: .normal)
         myCirclePostsFeedButton.setTitle("서클 게시물", for: .normal)
-        
+        topViewBottomSeparator.backgroundColor = UIColor.white.withAlphaComponent(0.05)
         myCirclePostsButtonBottomBar.isHidden = true
     }
     
     func addTargets() {
         myPersonalPostsFeedButton.addTarget(self, action: #selector(selectMyPostFeedButtonAction), for: .touchUpInside)
         myCirclePostsFeedButton.addTarget(self, action: #selector(selectMyPostFeedButtonAction), for: .touchUpInside)
-        settingListButton.addTarget(self, action: #selector(settingListButtonAction), for: .touchUpInside)
+        profileEditButton.addTarget(self, action: #selector(settingListButtonAction), for: .touchUpInside)
     }
     
     func uiViewUpdate() {
@@ -283,10 +160,22 @@ class BasicUserProfileViewController: UIViewController {
     }
     
     @objc func refreshData() {
-
+        
     }
     
     @objc func settingListButtonAction() {
+        
+    }
+        
+    @objc func profileEdditButtonAction() {
+        
+    }
+    
+    func navigationBarLayout() {
+
+    }
+    
+    @objc func backButtonAction() {
         
     }
 }
