01f3d34c58cc31d0d86a3089a1b420a4d5fd5be5
grgnjhyxxk <grgnjhyxxk@gmail.com>
2023-10-22 23:35:05 +0900

    [✏️  코드 작성] UserProfile View UI변경에 따른 코드 수정 및 Navigation Bar추가

--- Circle/Circle/ViewController/UserProfile/MyProfileViewController.swift
+++ Circle/Circle/ViewController/UserProfile/MyProfileViewController.swift
@@ -11,52 +11,138 @@ import SnapKit
 class MyProfileViewController: BasicUserProfileViewController {
     
     override func mainViewSetting() {
-        settingListButton.snp.makeConstraints { make in
-            make.trailing.equalToSuperview().offset(-25)
-            make.centerY.equalTo(profileNameTitleLabel).offset(2)
-            make.size.equalTo(CGSize(width: 25, height: 24))
-        }
-        
-        postingButton.snp.makeConstraints { make in
-            make.trailing.equalTo(settingListButton.snp.leading).offset(-15)
-            make.centerY.equalTo(profileNameTitleLabel)
-            make.size.equalTo(CGSize(width: 28, height: 28))
-        }
+
     }
     
     override func contentViewSetting() {
+        profileEditButton.snp.makeConstraints { make in
+            make.top.equalTo(userProfileImageView)
+            make.trailing.equalTo(-20)
+            make.size.equalTo(CGSize(width: 70, height: 30))
+        }
+        
+        topViewBottomSeparator.snp.makeConstraints { make in
+            make.leading.trailing.equalTo(contentView)
+            make.top.equalTo(subStatusButton.snp.bottom).offset(20)
+            make.height.equalTo(1)
+        }
+        
+        myPersonalPostsFeedButton.snp.makeConstraints { make in
+            make.top.equalTo(topViewBottomSeparator.snp.bottom)
+            make.leading.equalTo(contentView)
+            make.size.equalTo(CGSize(width: view.frame.width / 2, height: 40))
+        }
+        
+        myCirclePostsFeedButton.snp.makeConstraints { make in
+            make.top.equalTo(topViewBottomSeparator.snp.bottom)
+            make.trailing.equalTo(contentView)
+            make.size.equalTo(CGSize(width: view.frame.width / 2, height: 40))
+        }
+        
+        myPersonalPostsFeedButton.addSubview(myPersonalPostsFeedButtonBottomBar)
+        myCirclePostsFeedButton.addSubview(myCirclePostsButtonBottomBar)
 
+        myPersonalPostsFeedButtonBottomBar.snp.makeConstraints { make in
+            make.centerX.bottom.equalToSuperview()
+            make.size.equalTo(CGSize(width: 100, height: 2))
+        }
+        
+        myCirclePostsButtonBottomBar.snp.makeConstraints { make in
+            make.centerX.bottom.equalToSuperview()
+            make.size.equalTo(CGSize(width: 100, height: 2))
+        }
     }
     
     override func uiViewUpdate() {
         let userData = myDataList[0]
         
-        profileNameTitleLabel.text = userData.profileName
+//        profileNameTitleLabel.text = userData.profileName
+        profileNameButton.setTitle("\(userData.profileName)", for: .normal)
         userNameTitleLabel.text = userData.userName
-        introductionLabel.text = userData.intrduction
-        myCircleDigitsLabel.text = formatNumber(userData.myCircleDigits)
-        myInTheCircleDigitsLabel.text = formatNumber(userData.myInTheCircleDigits)
-        myPostDigitsLabel.text = formatNumber(userData.myPostDigits)
-        followerDigitsLabel.text = formatNumber(userData.followerDigits)
-        followingDigitsLabel.text = formatNumber(userData.followingDigits)
+        introductionLabel.text = userData.introduction
+        
+        let subStatusButtonString = "팔로우 \(formatNumber(userData.followerDigits))   팔로잉 \(formatNumber(userData.followingDigits))"
+        
+        let attributedsubStatusButtonString = NSMutableAttributedString(string: subStatusButtonString)
+        
+        let subStatusButtonStringRangeOne = (subStatusButtonString as NSString).range(of: "\(formatNumber(userData.followerDigits))")
+        let subStatusButtonStringRangeTwo = (subStatusButtonString as NSString).range(of: "\(formatNumber(userData.followingDigits))")
+
+        attributedsubStatusButtonString.addAttribute(.font, value: UIFont.systemFont(ofSize: 14, weight: .semibold), range: subStatusButtonStringRangeOne)
+        attributedsubStatusButtonString.addAttribute(.font, value: UIFont.systemFont(ofSize: 14, weight: .semibold), range: subStatusButtonStringRangeTwo)
+        
+        subStatusButton.setAttributedTitle(attributedsubStatusButtonString, for: .normal)
         
         if let imageString = userData.image {
             if let image = UIImage(named: imageString) {
                 userProfileImageView.image = image
             } else {
-                // 이미지를 로드할 수 없는 경우
                 userProfileImageView.image = UIImage(named: "BasicUserProfileImage")
             }
         } else {
-            // 이미지 정보가 없는 경우
             userProfileImageView.image = UIImage(named: "BasicUserProfileImage")
         }
         
         if userData.socialValidation {
-            socialValidationMarkButton.isHidden = false
+            if let image = UIImage(systemName: "checkmark.seal.fill")?.withConfiguration(UIImage.SymbolConfiguration(pointSize: 14, weight: .light)) {
+                profileNameButton.setImage(image, for: .normal)
+            }
+        
+            profileNameButton.tintColor = UIColor.systemBlue
+            profileNameButton.imageEdgeInsets = UIEdgeInsets(top: 1, left: 4.5, bottom: 0, right: 0)
+            profileNameButton.semanticContentAttribute = .forceRightToLeft
+            profileNameButton.contentVerticalAlignment = .center
+            profileNameButton.contentHorizontalAlignment = .center
         } else {
-            socialValidationMarkButton.isHidden = true
+            profileNameButton.setImage(nil, for: .normal)
         }
+    }
+
+    override func navigationBarLayout() {
+        let backButton = UIBarButtonItem(image: UIImage(systemName: "chevron.backward"), style: .plain, target: self, action: #selector(backButtonAction))
+        
+        let settingListBarButton = UIButton()
+        let postingBarButton = UIButton()
+
+        if let image = UIImage(systemName: "list.bullet") {
+            settingListBarButton.setImage(image, for: .normal)
+        }
+        
+        if let image = UIImage(systemName: "plus.app") {
+            postingBarButton.setImage(image, for: .normal)
+        }
+        
+        backButton.tintColor = UIColor.white
+        settingListBarButton.tintColor = UIColor.white
+        postingBarButton.tintColor = UIColor.white
+        settingListBarButton.contentHorizontalAlignment = .fill
+        settingListBarButton.contentVerticalAlignment = .fill
+        postingBarButton.contentHorizontalAlignment = .fill
+        postingBarButton.contentVerticalAlignment = .fill
+
+        profileNameButton.snp.makeConstraints { make in
+            make.width.equalTo(200)
+        }
+        
+        settingListBarButton.snp.makeConstraints { make in
+            make.size.equalTo(CGSize(width: 26, height: 24))
+        }
+        
+        postingBarButton.snp.makeConstraints { make in
+            make.size.equalTo(CGSize(width: 30, height: 27))
+        }
+
+        let righthStackview = UIStackView.init(arrangedSubviews: [postingBarButton, settingListBarButton])
+        righthStackview.distribution = .equalSpacing
+        righthStackview.axis = .horizontal
+        righthStackview.alignment = .center
+        righthStackview.spacing = 15
+
+        let rightStackBarButtonItem = UIBarButtonItem(customView: righthStackview)
+        
+        navigationItem.leftBarButtonItem = backButton
+        navigationItem.titleView = profileNameButton
+        navigationItem.rightBarButtonItem = rightStackBarButtonItem
     }
     
     @objc override func refreshData() {
@@ -73,6 +159,10 @@ class MyProfileViewController: BasicUserProfileViewController {
             }
             self.refreshControl.endRefreshing()
             self.uiViewUpdate()
+            self.mainViewSetting()
+            self.contentViewLayout()
+            self.contentViewSetting()
+            self.navigationBarLayout()
         }
     }
     
@@ -80,6 +170,10 @@ class MyProfileViewController: BasicUserProfileViewController {
         let viewController = SettingTableViewController()
         
         present(viewController, animated: true)
+    }
+    
+    @objc override func profileEdditButtonAction() {
+         
     }
 }
 
