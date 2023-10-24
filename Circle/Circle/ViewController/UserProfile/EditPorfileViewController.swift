cd7592ba8c26d6be697ce87e349f2e8b95209552
grgnjhyxxk <grgnjhyxxk@gmail.com>
2023-10-24 12:00:36 +0900

    [✏️ 코드 작성] 기본 ViewLayout 환경 추가

--- Circle/Circle/ViewController/UserProfile/EditPorfileViewController.swift
+++ Circle/Circle/ViewController/UserProfile/EditPorfileViewController.swift
@@ -5,4 +5,27 @@
 //  Created by Jaehyeok Lim on 10/24/23.
 //
 
-import Foundation
+import UIKit
+import SnapKit
+
+class EditPorfileViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = UIColor.black
+        navigationBarLayout()
+    }
+    
+    func navigationBarLayout() {
+        let backButton = UIBarButtonItem(image: UIImage(systemName: "chevron.backward"), style: .plain, target: self, action: #selector(backButtonAction))
+        
+        backButton.tintColor = UIColor.white
+        
+        navigationItem.leftBarButtonItem = backButton
+        
+        navigationItem.backBarButtonItem = UIBarButtonItem(title: "", style: .plain, target: nil, action: nil)
+    }
+    
+    @objc func backButtonAction() {
+        navigationController?.popViewController(animated: true)
+    }
+}
