c1c57bda12c3b576ff539996d2149538be83c62e
grgnjhyxxk <grgnjhyxxk@gmail.com>
2023-10-24 11:54:20 +0900

    [✏️  코드 작성] SearchResult 및 UserData 프로퍼티 추가 및 formatNumber 수정

--- Circle/Circle/DataManager.swift
+++ Circle/Circle/DataManager.swift
@@ -8,7 +8,7 @@
 import UIKit
 
 var myDataList = [UserData]()
-var searchUserList = [String]()
+var searchUserList = [SearchResult]()
 
 struct UserData {
     var profileName: String
@@ -20,30 +20,41 @@ struct UserData {
     var followerDigits: Int
     var followingDigits: Int
     var socialValidation: Bool
+    var backgroundImage: String?
+    var profileImage: String?
+    var userCategory: String?
     var introduction: String?
     var email: String?
     var phoneNumber: String?
-    var image: String?
     var birth: String?
     var gender: String?
     var userID: String?
 }
 
-func formatNumber(_ number: Int) -> String {
-    let formatter = NumberFormatter()
-    formatter.numberStyle = .decimal
-    formatter.maximumFractionDigits = 1
+struct SearchResult {
+    var profileName: String
+    var profileImage: String?
+    var userName: String
+    var socialValidation: Bool
+}
 
-    if number >= 1_000_000 {
-        let roundedValue = round(Double(number) / 100_000) / 10
-        return "\(formatter.string(from: NSNumber(value: roundedValue)) ?? "")만"
-    } else if number >= 1_000 {
-        let roundedValue = round(Double(number) / 100) / 10
-        return "\(formatter.string(from: NSNumber(value: roundedValue)) ?? "")천"
-    } else if number >= 100_000_000 {
-        let roundedValue = round(Double(number) / 100_000_000) / 10
-        return "\(formatter.string(from: NSNumber(value: roundedValue)) ?? "")억"
+func formatNumber(_ number: Int) -> String {
+    if number <= 9999 {
+        return String(number)
+    } else if number >= 10000 && number <= 9999999 {
+        let firstThreeDigits = String(number / 1000)
+        let fourthDigit = number % 1000 / 100
+        
+        if fourthDigit == 0 {
+            return firstThreeDigits + "만"
+        } else {
+            return firstThreeDigits + "." + String(fourthDigit) + "만"
+        }
+    } else if number >= 10000000 && number <= 99999999 {
+        let firstDigit = String(number / 10000000)
+        let remainingDigits = String(number % 10000000 / 10000)
+        return firstDigit + "," + remainingDigits + "만"
     } else {
-        return "\(number)"
+        return "숫자 범위를 벗어났습니다."
     }
 }
