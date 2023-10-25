c182c1d15a1363d615249307d22157f498660abf
grgnjhyxxk <grgnjhyxxk@gmail.com>
2023-10-25 22:45:28 +0900

    ✏️  코드 작성] EditProfileView속 TableView를 꾸밀 간단한 CustomCell  레이아웃 작성

--- Circle/Circle/Cell/EditProfileTableViewCell.swift
+++ Circle/Circle/Cell/EditProfileTableViewCell.swift
@@ -5,4 +5,54 @@
 //  Created by Jaehyeok Lim on 10/24/23.
 //
 
-import Foundation
+import UIKit
+import SnapKit
+
+class EditProfileTableViewCell: UITableViewCell {
+    
+    let titleLabel: UILabel = {
+        let label = UILabel()
+        
+        label.textColor = UIColor.white
+        label.font = UIFont.systemFont(ofSize: 16, weight: .regular)
+        
+        return label
+    }()
+    
+    let subLabel: UILabel = {
+        let label = UILabel()
+        
+        label.textColor = UIColor.white
+        label.font = UIFont.systemFont(ofSize: 16, weight: .regular)
+        label.numberOfLines = 0
+
+        return label
+    }()
+
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+//        contentView.backgroundColor = UIColor.black
+        contentView.addSubview(titleLabel)
+        contentView.addSubview(subLabel)
+
+        titleLabel.snp.makeConstraints { make in
+            make.centerY.equalToSuperview()
+            make.leading.equalToSuperview().offset(15)
+        }
+
+        subLabel.snp.makeConstraints { make in
+            make.centerY.equalToSuperview()
+            make.leading.equalToSuperview().offset(150)
+            make.trailing.equalTo(-15)
+        }
+        
+        contentView.snp.makeConstraints { make in
+            make.top.leading.trailing.equalToSuperview()
+            make.height.equalTo(subLabel.snp.height).offset(25)
+        }
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
