62fb92c39d13acdfb5521ca247a60501ee8566e1
grgnjhyxxk <grgnjhyxxk@gmail.com>
2023-10-31 23:30:44 +0900

    [✏️  코드 작성] createSegmentedControl 추가

--- Circle/Circle/View/MainView.swift
+++ Circle/Circle/View/MainView.swift
@@ -8,7 +8,6 @@
 import UIKit
 
 class MainView: UIView {
-    
     func MainViewTitleLabel() -> UILabel {
         let label = UILabel()
         
@@ -17,5 +16,26 @@ class MainView: UIView {
         label.font = UIFont(name: "NotoSans-Thin", size: 30)
         
         return label
+    }
+    
+    func createSegmentedControl() -> UISegmentedControl {
+        let segmentedControl = UISegmentedControl(items: ["팔로우 중", "가입 서클"])
+        
+        segmentedControl.selectedSegmentIndex = 0
+        
+        let image = UIImage()
+        segmentedControl.setBackgroundImage(image, for: .normal, barMetrics: .default)
+        segmentedControl.setBackgroundImage(image, for: .selected, barMetrics: .default)
+        segmentedControl.setBackgroundImage(image, for: .highlighted, barMetrics: .default)
+        
+        segmentedControl.setDividerImage(image, forLeftSegmentState: .selected, rightSegmentState: .normal, barMetrics: .default)
+        
+        let normalFont = UIFont.systemFont(ofSize: 16, weight: .medium)
+            segmentedControl.setTitleTextAttributes([NSAttributedString.Key.font: normalFont as Any, NSAttributedString.Key.foregroundColor: UIColor.placeholderText], for: .normal)
+        let selectedFont = UIFont.systemFont(ofSize: 16, weight: .semibold)
+        segmentedControl.setTitleTextAttributes([NSAttributedString.Key.font: selectedFont as Any, NSAttributedString.Key.foregroundColor: UIColor.white], for: .selected)
+        
+        
+        return segmentedControl
     }
 }
