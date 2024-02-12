6900224c099b95acf5a11946fa36ac29247e21e8
grgnjhyxxk <grgnjhyxxk@gmail.com>
2024-02-12 19:45:25 +0900

    데이터 업데이트와 테이블뷰와 하단 스크롤 작업 추가

--- Circle/Circle/Controller/MainViewController.swift
+++ Circle/Circle/Controller/MainViewController.swift
@@ -8,7 +8,8 @@
 import UIKit
 import SnapKit
 
-class MainViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+class MainViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, UIScrollViewDelegate {
+    
     private var viewList: [UIView] = []
     private var contentViewList: [UIView] = []
     
@@ -16,14 +17,19 @@ class MainViewController: UIViewController, UITableViewDelegate, UITableViewData
     private var contentView: UIView = UIView()
     private var separator: UIView = IntroView().separator()
     private var spinningCirclesView = SpinningCirclesView()
-    //    private var followingPostsCollectionView: UICollectionView = UICollectionView(frame: CGRect.zero, collectionViewLayout: UICollectionViewFlowLayout())
     
     private let navigationTitleViewLabel: UILabel = IntroView().introMainTitleLabel()
     
     private var segmentedControl: UISegmentedControl = MainView().createSegmentedControl()
     
     let followingPostsTableView: UITableView = UITableView()
-
+    
+    lazy var refreshControl: UIRefreshControl = {
+        let refreshControl = UIRefreshControl()
+        refreshControl.addTarget(self, action: #selector(refreshData), for: .valueChanged)
+        return refreshControl
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -37,13 +43,16 @@ class MainViewController: UIViewController, UITableViewDelegate, UITableViewData
         
         followingPostsTableView.dataSource = self
         followingPostsTableView.delegate = self
-        
+
         navigationController?.hidesBarsOnSwipe = true
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-        spinningCirclesView.startAnimation()
+        
+        DispatchQueue.main.async {
+            self.followingPostsTableView.reloadData()
+        }
     }
     
     private func addOnView() {
@@ -55,47 +64,15 @@ class MainViewController: UIViewController, UITableViewDelegate, UITableViewData
     }
     
     private func viewLayout() {
-        view.backgroundColor = UIColor.black
+        view.backgroundColor = UIColor.clear
         followingPostsTableView.backgroundColor = UIColor.black
-//        scrollView.backgroundColor = UIColor.black
-//        contentView.backgroundColor = UIColor.black
+        followingPostsTableView.refreshControl = refreshControl
         
-//        scrollView.snp.makeConstraints { make in
-//            make.top.equalToSuperview()
-//            make.leading.trailing.bottom.equalToSuperview()
-//        }
-        
-//        segmentedControl.snp.makeConstraints { make in
-//            make.top.equalTo(view.safeAreaLayoutGuide.snp.top).offset(10)
-//            make.centerX.equalToSuperview()
-//            make.width.equalToSuperview()
-//            make.height.equalTo(30)
-//        }
-        
-//        separator.snp.makeConstraints { make in
-//            make.top.equalTo(segmentedControl.snp.bottom).offset(15)
-//            make.width.equalToSuperview()
-//            make.height.equalTo(1)
-//        }
-        
-//        contentView.snp.makeConstraints { make in
-//            make.edges.equalToSuperview()
-//            make.width.equalToSuperview()
-//            make.height.equalTo(1000)
-//        }
-        
-        scrollView.showsVerticalScrollIndicator = false
         navigationController?.navigationBar.isTranslucent = false
         navigationController?.navigationBar.shadowImage = UIImage()
     }
     
     private func addOnContentView() {
-//        contentViewList = [followingPostsCollectionView]
-//        
-//        for uiView in contentViewList {
-//            contentView.addSubview(uiView)
-//        }
-        
         followingPostsTableView.snp.makeConstraints { make in
             make.top.equalToSuperview()
             make.leading.trailing.bottom.equalToSuperview()
@@ -103,7 +80,8 @@ class MainViewController: UIViewController, UITableViewDelegate, UITableViewData
     }
     
     private func contentViewLayout() {
-        followingPostsTableView.register(FollowingPostsCollectionViewCell.self, forCellReuseIdentifier: "FollowingPostsCollectionViewCell")
+        followingPostsTableView.register(FollowingPostsTableViewCell.self, forCellReuseIdentifier: "FollowingPostsTableViewCell")
+        followingPostsTableView.register(FollowingPostsTableViewCell2.self, forCellReuseIdentifier: "FollowingPostsTableViewCell2")
     }
     
     private func navigationBarLayout() {
@@ -124,7 +102,7 @@ class MainViewController: UIViewController, UITableViewDelegate, UITableViewData
         alarmBarButton.contentVerticalAlignment = .fill
         boltBarButton.contentHorizontalAlignment = .fill
         boltBarButton.contentVerticalAlignment = .fill
-                
+        
         alarmBarButton.snp.makeConstraints { make in
             make.size.equalTo(CGSize(width: 27, height: 27))
         }
@@ -138,17 +116,17 @@ class MainViewController: UIViewController, UITableViewDelegate, UITableViewData
         righthStackview.axis = .horizontal
         righthStackview.alignment = .center
         righthStackview.spacing = 15
-
+        
         let rightStackBarButtonItem = UIBarButtonItem(customView: righthStackview)
         
-//        navigationController?.navigationBar.backgroundColor = UIColor.black
-        
         spinningCirclesView.setCircleSizes(bigCircleSize: 18.5, smallCircleSize: 4.5, radius: 15.5)
-        navigationTitleViewLabel.text = "C"
+        navigationTitleViewLabel.text = "Circles"
         navigationTitleViewLabel.font = UIFont(name: "PetitFormalScript-Regular", size: 28)
         
         navigationItem.rightBarButtonItem = rightStackBarButtonItem
-        navigationItem.titleView = spinningCirclesView
+        navigationItem.titleView = navigationTitleViewLabel
+        
+        followingPostsTableView.separatorInset.left = 0
     }
     
     private func setupSwipeGesture() {
@@ -192,20 +170,100 @@ class MainViewController: UIViewController, UITableViewDelegate, UITableViewData
         }
     }
     
+    @objc private func refreshData() {
+        SharedPostModel.othersPosts.removeAll()
+
+        retrieveFirstFourPosts { error in
+            if let error = error {
+                print("Failed to retrieve all posts:", error.localizedDescription)
+            } else {
+                print("All posts retrieved successfully")
+                
+                DispatchQueue.main.async {
+                    // Reload table view or perform any other UI updates here
+                    self.followingPostsTableView.reloadData()
+                    
+                    // End refreshing after updating UI
+                    self.refreshControl.endRefreshing()
+                }
+            }
+        }
+    }
+
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        let offsetY = scrollView.contentOffset.y
+        let contentHeight = scrollView.contentSize.height
+        let boundsHeight = scrollView.bounds.size.height
+
+        // 스크롤이 맨 아래로 도달했을 때 리프레쉬 실행
+        if offsetY > contentHeight - boundsHeight {
+            if !refreshControl.isRefreshing {
+                refreshControl.beginRefreshing()
+
+                retrieveNextFourPosts { error in
+                    if let error = error {
+                        print("Failed to retrieve all posts:", error.localizedDescription)
+                    } else {
+                        print("All posts retrieved successfully")
+                        
+                        DispatchQueue.main.async {
+                            // Reload table view or perform any other UI updates here
+                            self.followingPostsTableView.reloadData()
+                            
+                            // End refreshing after updating UI
+                            self.refreshControl.endRefreshing()
+                        }
+                    }
+                }
+            }
+        }
+    }
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+        return SharedPostModel.othersPosts.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "FollowingPostsCollectionViewCell", for: indexPath) as!
-        FollowingPostsCollectionViewCell
+        let post = SharedPostModel.othersPosts[indexPath.row]
         
-        
-        return cell
+        if post.images?.isEmpty == true {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "FollowingPostsTableViewCell2", for: indexPath) as! FollowingPostsTableViewCell2
+
+            cell.feedTextLabel.text = post.content
+
+            // 게시글 작성자의 프로필 정보 가져오기
+            if let userID = post.userID,
+               let userProfile = SharedProfileModel.postsProfile.first(where: { $0.userID == userID }) {
+                cell.profileImageView.image = userProfile.profileImage
+                cell.profileNameLabel.text = userProfile.profileName
+                cell.userNameLabel.text = userProfile.userName
+                cell.socialValidationImageView.isHidden = !(userProfile.socialValidation ?? false)
+            }
+
+            return cell
+        } else {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "FollowingPostsTableViewCell", for: indexPath) as! FollowingPostsTableViewCell
+
+            cell.images = post.images
+            cell.feedTextLabel.text = post.content
+
+            // 게시글 작성자의 프로필 정보 가져오기
+            if let userID = post.userID,
+               let userProfile = SharedProfileModel.postsProfile.first(where: { $0.userID == userID }) {
+                cell.profileImageView.image = userProfile.profileImage
+                cell.profileNameLabel.text = userProfile.profileName
+                cell.userNameLabel.text = userProfile.userName
+                cell.socialValidationImageView.isHidden = !(userProfile.socialValidation ?? false)
+            }
+
+            cell.collectionView.reloadData()
+            followingPostsTableView.layoutIfNeeded()
+
+            return cell
+        }
     }
-        
+    
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         tableView.deselectRow(at: indexPath, animated: true)
     }
 }
-
