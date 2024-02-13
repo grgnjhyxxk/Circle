47cbb06df29c8808c1c0636f11752eb441438f63
grgnjhyxxk <grgnjhyxxk@gmail.com>
2024-02-13 23:46:15 +0900

    클래스 통합

--- Circle/Circle/Cell/FollowingPostsCollectionViewCell.swift
+++ Circle/Circle/Cell/FollowingPostsCollectionViewCell.swift
@@ -219,12 +219,24 @@ class FollowingPostsTableViewCell: BaseFollowingPostsTableViewCell, UICollection
         collectionView.backgroundColor = .black
         return collectionView
     }()
+    
+    var images: [UIImage]? {
+        didSet {
+            if let images = images, !images.isEmpty {
+                collectionView.snp.updateConstraints { make in
+                    make.height.equalTo(200) // 높이 값을 원하는 값으로 설정
+                }
+            } else {
+                collectionView.snp.updateConstraints { make in
+                    make.height.equalTo(0)
+                }
+            }
 
-//    var index = 0
-//    var num = 0
-    
-    var images: [UIImage]?
-    
+            collectionView.reloadData()
+            contentView.layoutIfNeeded()
+        }
+    }
+
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
         addOnContentView()
@@ -239,7 +251,11 @@ class FollowingPostsTableViewCell: BaseFollowingPostsTableViewCell, UICollection
     }
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return images!.count
+        if let images = images {
+            return images.count
+        } else {
+            return 0
+        }
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
@@ -255,7 +271,7 @@ class FollowingPostsTableViewCell: BaseFollowingPostsTableViewCell, UICollection
         let image = images![indexPath.row]
         let count = images!.count
         let screenWidth = collectionView.bounds.width  // 화면의 너비
-
+        
         if count == 1 {
             let aspectRatio = image.size.width / image.size.height
             let cellWidth = min(image.size.width, screenWidth)
@@ -327,13 +343,6 @@ class FollowingPostsTableViewCell: BaseFollowingPostsTableViewCell, UICollection
             make.trailing.equalToSuperview().offset(-15)
             make.bottom.equalTo(collectionView.snp.top).offset(-7)
         }
-        
-//        feedTextLabel.snp.makeConstraints { make in
-//            make.top.equalTo(topView.snp.bottom).offset(10)
-//            make.leading.equalTo(60)
-//            make.trailing.equalToSuperview().offset(-15)
-//            make.bottom.equalTo(bottomView.snp.top).offset(-10)
-//        }
         
         collectionView.snp.makeConstraints { make in
             make.top.equalTo(feedTextLabel.snp.bottom).offset(10)
@@ -407,321 +416,5 @@ class FollowingPostsTableViewCell: BaseFollowingPostsTableViewCell, UICollection
             make.size.equalTo(CGSize(width: 18, height: 18))
         }
 
-    }
-}
-
-class FollowingPostsTableViewCell2: BaseFollowingPostsTableViewCell {
-    
-    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
-        super.init(style: style, reuseIdentifier: reuseIdentifier)
-        addOnContentView()
-        contentViewLayout()
-    }
-    
-    required init?(coder aDecoder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-    
-    override func contentViewLayout() {
-        topView.snp.makeConstraints { make in
-            make.top.equalToSuperview().offset(10)
-            make.width.equalToSuperview()
-            make.height.equalTo(40)
-        }
-        
-        profileImageView.snp.makeConstraints { make in
-            make.centerY.equalToSuperview()
-            make.leading.equalToSuperview().offset(10)
-            make.size.equalTo(CGSize(width: 40, height: 40))
-        }
-        
-        profileNameLabel.snp.makeConstraints { make in
-            make.top.equalTo(profileImageView)
-            make.leading.equalTo(profileImageView.snp.trailing).offset(10)
-        }
-        
-        moreButton.snp.makeConstraints { make in
-            make.trailing.equalToSuperview().offset(-10)
-            make.top.equalTo(profileNameLabel)
-        }
-        
-        userNameLabel.snp.makeConstraints { make in
-            make.centerY.equalToSuperview().offset(8)
-            make.leading.equalTo(profileImageView.snp.trailing).offset(10)
-        }
-
-        socialValidationImageView.snp.makeConstraints { make in
-            make.centerY.equalTo(profileNameLabel)
-            make.leading.equalTo(profileNameLabel.snp.trailing).offset(2)
-        }
-        
-        feedTextLabel.snp.makeConstraints { make in
-            make.top.equalTo(topView.snp.bottom)
-            make.leading.equalTo(60)
-            make.trailing.equalToSuperview().offset(-15)
-            make.bottom.equalTo(bottomView.snp.top)
-        }
-        
-        bottomView.snp.makeConstraints { make in
-            make.bottom.equalToSuperview()
-            make.width.equalToSuperview()
-            make.height.equalTo(40)
-        }
-        
-        likeButton.snp.makeConstraints { make in
-            make.centerY.equalTo(bottomView)
-            make.leading.equalTo(58)
-            make.width.equalTo(23)
-        }
-        
-        likeDigitsButton.snp.makeConstraints { make in
-            make.centerY.equalTo(bottomView)
-            make.leading.equalTo(likeButton.snp.trailing).offset(0)
-            make.width.equalTo(18)
-            make.height.equalTo(18)
-        }
-        
-        commentButton.snp.makeConstraints { make in
-            make.centerY.equalTo(bottomView)
-            make.leading.equalTo(likeDigitsButton.snp.trailing).offset(30)
-            make.width.equalTo(23)
-        }
-        
-        commentDigitsButton.snp.makeConstraints { make in
-            make.centerY.equalTo(bottomView)
-            make.leading.equalTo(commentButton.snp.trailing).offset(0)
-            make.width.equalTo(18)
-            make.height.equalTo(18)
-        }
-        
-        rewriteButton.snp.makeConstraints { make in
-            make.centerY.equalTo(bottomView)
-            make.leading.equalTo(commentDigitsButton.snp.trailing).offset(30)
-            make.width.equalTo(23)
-        }
-        
-        rewriteButtonDigitsButton.snp.makeConstraints { make in
-            make.centerY.equalTo(bottomView)
-            make.leading.equalTo(rewriteButton.snp.trailing).offset(0)
-            make.width.equalTo(18)
-            make.height.equalTo(18)
-        }
-        
-        savePostButton.snp.makeConstraints { make in
-            make.centerY.equalTo(bottomView)
-            make.leading.equalTo(rewriteButtonDigitsButton.snp.trailing).offset(30)
-            make.width.equalTo(23)
-        }
-        
-        savePostButtonDigitsButton.snp.makeConstraints { make in
-            make.centerY.equalTo(bottomView)
-            make.leading.equalTo(savePostButton.snp.trailing).offset(0)
-            make.width.equalTo(18)
-            make.height.equalTo(18)
-        }
-        
-        saveButton.snp.makeConstraints { make in
-            make.centerY.equalTo(bottomView)
-            make.trailing.equalToSuperview().offset(-10)
-            make.size.equalTo(CGSize(width: 18, height: 18))
-        }
-    }
-}
-
-class FollowingPostsTableViewCell3: BaseFollowingPostsTableViewCell, UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout {
-    
-    lazy var collectionView: UICollectionView = {
-        let layout = UICollectionViewFlowLayout()
-        layout.scrollDirection = .horizontal
-        
-        collectionView = UICollectionView(frame: contentView.bounds, collectionViewLayout: layout)
-        collectionView.register(PostViewImageCollectionViewCell.self, forCellWithReuseIdentifier: "PostViewImageCollectionViewCell")
-        collectionView.backgroundColor = .black
-        return collectionView
-    }()
-
-    var images: [UIImage]?
-    
-    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
-        super.init(style: style, reuseIdentifier: reuseIdentifier)
-        addOnContentView()
-        contentView.addSubview(collectionView)
-        contentViewLayout()
-        collectionView.delegate = self
-        collectionView.dataSource = self
-    }
-    
-    required init?(coder aDecoder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return images!.count
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "PostViewImageCollectionViewCell", for: indexPath) as? PostViewImageCollectionViewCell else {
-            return UICollectionViewCell()
-        }
-        cell.imageView.image = images![indexPath.row]
-        cell.deleteButton.isHidden = true
-        return cell
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        let image = images![indexPath.row]
-        let count = images!.count
-        let screenWidth = collectionView.bounds.width  // 화면의 너비
-
-        if count == 1 {
-            let aspectRatio = image.size.width / image.size.height
-            let cellWidth = min(image.size.width, screenWidth)
-            let cellHeight = cellWidth / aspectRatio
-
-            collectionView.snp.updateConstraints { make in
-                make.height.equalTo(cellHeight)
-            }
-            
-            collectionView.layoutIfNeeded()
-            
-            return CGSize(width: cellWidth, height: cellHeight)
-            
-        }  else {
-            let aspectRatio = image.size.width / image.size.height // 이미지의 가로 세로 비율
-            let cellHeight: CGFloat = 200 // 셀의 높이 고정
-            let cellWidth = cellHeight * aspectRatio // 이미지 비율을 유지한 너비 계산
-            
-            collectionView.snp.updateConstraints { make in
-                make.height.equalTo(cellHeight)
-            }
-            
-            collectionView.layoutIfNeeded()
-            
-            return CGSize(width: cellWidth, height: cellHeight)
-        }
-    }
-    
-    override func contentViewLayout() {
-        super.contentViewLayout()
-        
-        contentView.backgroundColor = UIColor.black
-                
-        topView.snp.makeConstraints { make in
-            make.top.equalToSuperview().offset(10)
-            make.width.equalToSuperview()
-            make.height.equalTo(40)
-        }
-        
-        profileImageView.snp.makeConstraints { make in
-            make.centerY.equalToSuperview()
-            make.leading.equalToSuperview().offset(10)
-            make.size.equalTo(CGSize(width: 40, height: 40))
-        }
-        
-        profileNameLabel.snp.makeConstraints { make in
-            make.top.equalTo(profileImageView)
-            make.leading.equalTo(profileImageView.snp.trailing).offset(10)
-        }
-        
-        moreButton.snp.makeConstraints { make in
-            make.trailing.equalToSuperview().offset(-10)
-            make.top.equalTo(profileNameLabel)
-        }
-        
-        userNameLabel.snp.makeConstraints { make in
-            make.centerY.equalToSuperview().offset(8)
-            make.leading.equalTo(profileImageView.snp.trailing).offset(10)
-        }
-
-        socialValidationImageView.snp.makeConstraints { make in
-            make.centerY.equalTo(profileNameLabel)
-            make.leading.equalTo(profileNameLabel.snp.trailing).offset(2)
-        }
-        
-        feedTextLabel.snp.makeConstraints { make in
-            make.top.equalTo(topView.snp.bottom)
-            make.leading.equalTo(60)
-            make.trailing.equalToSuperview().offset(-15)
-            make.bottom.equalTo(collectionView.snp.top).offset(-7)
-        }
-        
-//        feedTextLabel.snp.makeConstraints { make in
-//            make.top.equalTo(topView.snp.bottom).offset(10)
-//            make.leading.equalTo(60)
-//            make.trailing.equalToSuperview().offset(-15)
-//            make.bottom.equalTo(bottomView.snp.top).offset(-10)
-//        }
-        
-        collectionView.snp.makeConstraints { make in
-            make.top.equalTo(feedTextLabel.snp.bottom).offset(10)
-            make.leading.equalTo(60)
-            make.trailing.equalTo(-15)
-            make.bottom.equalTo(bottomView.snp.top)
-            make.height.equalTo(200)
-        }
-        
-        bottomView.snp.makeConstraints { make in
-            make.bottom.equalToSuperview()
-            make.width.equalToSuperview()
-            make.height.equalTo(40)
-        }
-        
-        likeButton.snp.makeConstraints { make in
-            make.centerY.equalTo(bottomView)
-            make.leading.equalTo(58)
-            make.width.equalTo(23)
-        }
-        
-        likeDigitsButton.snp.makeConstraints { make in
-            make.centerY.equalTo(bottomView)
-            make.leading.equalTo(likeButton.snp.trailing).offset(0)
-            make.width.equalTo(18)
-            make.height.equalTo(18)
-        }
-        
-        commentButton.snp.makeConstraints { make in
-            make.centerY.equalTo(bottomView)
-            make.leading.equalTo(likeDigitsButton.snp.trailing).offset(30)
-            make.width.equalTo(23)
-        }
-        
-        commentDigitsButton.snp.makeConstraints { make in
-            make.centerY.equalTo(bottomView)
-            make.leading.equalTo(commentButton.snp.trailing).offset(0)
-            make.width.equalTo(18)
-            make.height.equalTo(18)
-        }
-        
-        rewriteButton.snp.makeConstraints { make in
-            make.centerY.equalTo(bottomView)
-            make.leading.equalTo(commentDigitsButton.snp.trailing).offset(30)
-            make.width.equalTo(23)
-        }
-        
-        rewriteButtonDigitsButton.snp.makeConstraints { make in
-            make.centerY.equalTo(bottomView)
-            make.leading.equalTo(rewriteButton.snp.trailing).offset(0)
-            make.width.equalTo(18)
-            make.height.equalTo(18)
-        }
-        
-        savePostButton.snp.makeConstraints { make in
-            make.centerY.equalTo(bottomView)
-            make.leading.equalTo(rewriteButtonDigitsButton.snp.trailing).offset(30)
-            make.width.equalTo(23)
-        }
-        
-        savePostButtonDigitsButton.snp.makeConstraints { make in
-            make.centerY.equalTo(bottomView)
-            make.leading.equalTo(savePostButton.snp.trailing).offset(0)
-            make.width.equalTo(18)
-            make.height.equalTo(18)
-        }
-        
-        saveButton.snp.makeConstraints { make in
-            make.centerY.equalTo(bottomView)
-            make.trailing.equalToSuperview().offset(-10)
-            make.size.equalTo(CGSize(width: 18, height: 18))
-        }
     }
 }
