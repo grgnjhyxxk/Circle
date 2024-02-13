efec95b5a209f202c996beadb64165ae75366383
grgnjhyxxk <grgnjhyxxk@gmail.com>
2024-02-13 23:48:45 +0900

    refresh function 기능 수정 및 Cell 통합으로 인한 delegate 수정

--- Circle/Circle/Controller/MainViewController.swift
+++ Circle/Circle/Controller/MainViewController.swift
@@ -81,7 +81,6 @@ class MainViewController: UIViewController, UITableViewDelegate, UITableViewData
     
     private func contentViewLayout() {
         followingPostsTableView.register(FollowingPostsTableViewCell.self, forCellReuseIdentifier: "FollowingPostsTableViewCell")
-        followingPostsTableView.register(FollowingPostsTableViewCell2.self, forCellReuseIdentifier: "FollowingPostsTableViewCell2")
     }
     
     private func navigationBarLayout() {
@@ -176,6 +175,8 @@ class MainViewController: UIViewController, UITableViewDelegate, UITableViewData
         retrieveFirstFourPosts { error in
             if let error = error {
                 print("Failed to retrieve all posts:", error.localizedDescription)
+                
+                self.refreshControl.endRefreshing()
             } else {
                 print("All posts retrieved successfully")
                 
@@ -190,35 +191,71 @@ class MainViewController: UIViewController, UITableViewDelegate, UITableViewData
         }
     }
 
+//    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+//        let offsetY = scrollView.contentOffset.y
+//        let contentHeight = scrollView.contentSize.height
+//        let boundsHeight = scrollView.bounds.size.height
+//
+//        // 스크롤이 맨 아래로 도달했을 때 리프레쉬 실행
+//        if offsetY > contentHeight - boundsHeight {
+//            if !refreshControl.isRefreshing {
+//                refreshControl.beginRefreshing()
+//
+//                retrieveNextFourPosts { posts, error in
+//                    if let error = error {
+//                        print("Failed to retrieve all posts:", error.localizedDescription)
+//                    } else {
+//                        print("All posts retrieved successfully")
+//                        
+//                        DispatchQueue.main.async {
+//                            // Add new posts to the table view
+//                            let indexPaths = (SharedPostModel.othersPosts.count - posts.count ..< SharedPostModel.othersPosts.count).map { IndexPath(row: $0, section: 0) }
+//                            self.followingPostsTableView.insertRows(at: indexPaths, with: .automatic)
+//                            
+//                            // End refreshing after updating UI
+//                            self.refreshControl.endRefreshing()
+//                        }
+//                    }
+//                }
+//            }
+//        }
+//    }
+    
     func scrollViewDidScroll(_ scrollView: UIScrollView) {
         let offsetY = scrollView.contentOffset.y
         let contentHeight = scrollView.contentSize.height
         let boundsHeight = scrollView.bounds.size.height
+        
+        let beforeCount = SharedPostModel.othersPosts.count
 
-        // 스크롤이 맨 아래로 도달했을 때 리프레쉬 실행
+       // 스크롤이 맨 아래로 도달했을 때 리프레쉬 실행
         if offsetY > contentHeight - boundsHeight {
             if !refreshControl.isRefreshing {
-                refreshControl.beginRefreshing()
+               refreshControl.beginRefreshing()
 
-                retrieveNextFourPosts { error in
-                    if let error = error {
-                        print("Failed to retrieve all posts:", error.localizedDescription)
-                    } else {
-                        print("All posts retrieved successfully")
-                        
-                        DispatchQueue.main.async {
-                            // Reload table view or perform any other UI updates here
-                            self.followingPostsTableView.reloadData()
-                            
-                            // End refreshing after updating UI
-                            self.refreshControl.endRefreshing()
-                        }
-                    }
+               retrieveNextFourPosts { error in
+                   if let error = error {
+                       print("Failed to retrieve all posts:", error.localizedDescription)
+                       self.refreshControl.endRefreshing()
+                   } else {
+                       let afterCount = SharedPostModel.othersPosts.count
+                       
+                       if afterCount > beforeCount {
+                           print("All posts retrieved successfully")
+                           
+                           DispatchQueue.main.async {
+                               self.followingPostsTableView.reloadData()
+                               self.refreshControl.endRefreshing()
+                           }
+                       } else {
+                           self.refreshControl.endRefreshing()
+                       }
+                   }
                 }
             }
-        }
-    }
-    
+       }
+   }
+
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return SharedPostModel.othersPosts.count
     }
@@ -226,41 +263,24 @@ class MainViewController: UIViewController, UITableViewDelegate, UITableViewData
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let post = SharedPostModel.othersPosts[indexPath.row]
         
-        if post.images?.isEmpty == true {
-            let cell = tableView.dequeueReusableCell(withIdentifier: "FollowingPostsTableViewCell2", for: indexPath) as! FollowingPostsTableViewCell2
+        let cell = tableView.dequeueReusableCell(withIdentifier: "FollowingPostsTableViewCell", for: indexPath) as! FollowingPostsTableViewCell
+        
+        cell.images = post.images
+        cell.feedTextLabel.text = post.content
 
-            cell.feedTextLabel.text = post.content
-
-            // 게시글 작성자의 프로필 정보 가져오기
-            if let userID = post.userID,
-               let userProfile = SharedProfileModel.postsProfile.first(where: { $0.userID == userID }) {
-                cell.profileImageView.image = userProfile.profileImage
-                cell.profileNameLabel.text = userProfile.profileName
-                cell.userNameLabel.text = userProfile.userName
-                cell.socialValidationImageView.isHidden = !(userProfile.socialValidation ?? false)
-            }
-
-            return cell
-        } else {
-            let cell = tableView.dequeueReusableCell(withIdentifier: "FollowingPostsTableViewCell", for: indexPath) as! FollowingPostsTableViewCell
-
-            cell.images = post.images
-            cell.feedTextLabel.text = post.content
-
-            // 게시글 작성자의 프로필 정보 가져오기
-            if let userID = post.userID,
-               let userProfile = SharedProfileModel.postsProfile.first(where: { $0.userID == userID }) {
-                cell.profileImageView.image = userProfile.profileImage
-                cell.profileNameLabel.text = userProfile.profileName
-                cell.userNameLabel.text = userProfile.userName
-                cell.socialValidationImageView.isHidden = !(userProfile.socialValidation ?? false)
-            }
-
-            cell.collectionView.reloadData()
-            followingPostsTableView.layoutIfNeeded()
-
-            return cell
+        // 게시글 작성자의 프로필 정보 가져오기
+        if let userID = post.userID,
+           let userProfile = SharedProfileModel.postsProfile.first(where: { $0.userID == userID }) {
+            cell.profileImageView.image = userProfile.profileImage
+            cell.profileNameLabel.text = userProfile.profileName
+            cell.userNameLabel.text = userProfile.userName
+            cell.socialValidationImageView.isHidden = !(userProfile.socialValidation ?? false)
         }
+
+        cell.collectionView.reloadData()
+        followingPostsTableView.layoutIfNeeded()
+        
+        return cell
     }
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
