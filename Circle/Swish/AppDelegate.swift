719b45589b784be0214d82b221be813feb5bae95
grgnjhyxxk <grgnjhyxxk@gmail.com>
2024-04-24 16:16:21 +0900

    코드 정렬

--- Circle/Swish/AppDelegate.swift
+++ Circle/Swish/AppDelegate.swift
@@ -9,6 +9,7 @@ import UIKit
 import CoreData
 
 @main
+
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
@@ -41,7 +42,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
          application to it. This property is optional since there are legitimate
          error conditions that could cause the creation of the store to fail.
         */
-        let container = NSPersistentCloudKitContainer(name: "Circle")
+        let container = NSPersistentCloudKitContainer(name: "Swish")
         container.loadPersistentStores(completionHandler: { (storeDescription, error) in
             if let error = error as NSError? {
                 // Replace this implementation with code to handle the error appropriately.
