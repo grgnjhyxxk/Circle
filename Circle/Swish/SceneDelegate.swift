ac53e2fdeabdd264a8e4a7200e3aef8c4b68f826
grgnjhyxxk <grgnjhyxxk@gmail.com>
2024-04-24 16:16:34 +0900

    자동로그인 기능 구현

--- Circle/Swish/SceneDelegate.swift
+++ Circle/Swish/SceneDelegate.swift
@@ -18,55 +18,77 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         window = UIWindow(windowScene: windowScene)
         
-        // UserDefaults에서 로그인 상태, 아이디, 비밀번호 확인
         let loginState = UserDefaults.standard.bool(forKey: "loginState")
-        let id = UserDefaults.standard.string(forKey: "profileName")
-        let password = UserDefaults.standard.string(forKey: "password")
-
-        if loginState {
-            let dispatchGroup = DispatchGroup()
-            
-            dispatchGroup.enter()
-            fetchUserData(profileName: id!) { error in
-                defer {
-                    dispatchGroup.leave()
-                }
-                if let error = error {
-                    print("Error fetching user data: \(error.localizedDescription)")
-                }
-            }
-            
-            dispatchGroup.enter()
-            retrieveFirstFourPosts { error in
-                defer {
-                    dispatchGroup.leave()
-                }
-                if let error = error {
-                    print("Error fetching first four posts: \(error.localizedDescription)")
-                } else {
-                    print("First four posts retrieved successfully")
-                }
-            }
-            
-            dispatchGroup.notify(queue: DispatchQueue.main) {
-                print("All asynchronous tasks are completed. Continue with the next steps.")
+        
+        if let id = UserDefaults.standard.string(forKey: "userID") {
+            if loginState {
+                let dispatchGroup = DispatchGroup()
                 
-                let myProfile = SharedProfileModel.myProfile
-                
-                retrieveMyPosts(userID: myProfile.userID!) { (error) in
+                dispatchGroup.enter()
+                fetchUserDataUseUserID(userID: id) { error in
+                    defer {
+                        dispatchGroup.leave()
+                    }
                     if let error = error {
-                        print("Error retrieving user posts: \(error.localizedDescription)")
-                    } else {
-                        let rootViewController = TabBarController()
-                        rootViewController.modalPresentationStyle = .fullScreen
-                        self.window?.rootViewController = rootViewController
+                        print("Error fetching user data: \(error.localizedDescription)")
+                        self.window?.rootViewController = IntroViewController().isLoggedIn()
                         self.window?.makeKeyAndVisible()
                     }
                 }
+                
+                dispatchGroup.enter()
+                retrieveFirstFourPosts { error in
+                    defer {
+                        dispatchGroup.leave()
+                    }
+                    if let error = error {
+                        print("Error fetching first four posts: \(error.localizedDescription)")
+                    } else {
+                        print("First four posts retrieved successfully")
+                    }
+                }
+                
+                dispatchGroup.notify(queue: DispatchQueue.main) {
+                    print("All asynchronous tasks are completed. Continue with the next steps.")
+                    
+                    let myProfile = SharedProfileModel.myProfile
+                    
+                    retrieveMyPosts(userID: myProfile.userID!) { (error) in
+                        if let error = error {
+                            print("Error retrieving user posts: \(error.localizedDescription)")
+                        } else {
+                            fetchRecentSearchesRecords { error in
+                                if let error = error {
+                                    print("Error fetching first four posts: \(error.localizedDescription)")
+                                } else {
+                                    print("RecentSearchesRecords retrieved successfully")
+                                    
+                                    // 최근 검색 기록에서 "user" 타입인 데이터를 가져옴
+                                    for record in SharedRecentSearchesRecordModel.nomarl {
+                                        if record.searchesType == "user" {
+                                            fetchUserData2(userID: record.searchesData ?? "") { error in
+                                                if let error = error {
+                                                    print("Error fetching user data: \(error)")
+                                                } else {
+                                                    
+                                                }
+                                            }
+                                        }
+                                    }
+                                    
+                                    let rootViewController = TabBarController()
+                                    rootViewController.modalPresentationStyle = .fullScreen
+                                    self.window?.rootViewController = rootViewController
+                                    self.window?.makeKeyAndVisible()
+                                }
+                            }
+                        }
+                    }
+                }
+            } else {
+                window?.rootViewController = IntroViewController().isLoggedIn()
+                window?.makeKeyAndVisible()
             }
-        } else {
-            window?.rootViewController = IntroViewController().isLoggedIn()
-            window?.makeKeyAndVisible()
         }
     }
 
